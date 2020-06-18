fc18b05e962efefa7ae32ce97a268284214ef2b4
Astamur Kirillin <astamur.kirillin@gmail.com>
2020-06-18 18:48:17 +0300

    Added homework for Lesson 1

---
+++ src/main/java/dev/astamur/geekbrains/lessons/lesson1/homework/AbstractAthlete.java
@@ -0,0 +1,17 @@
+package dev.astamur.geekbrains.lessons.lesson1.homework;
+
+public abstract class AbstractAthlete implements Athlete {
+    protected final String name;
+    protected final double maxRunDistance;
+    protected final double maxJumpHeight;
+
+    public AbstractAthlete(String name, double maxRunDistance, double maxJumpHeight) {
+        this.name = name;
+        this.maxRunDistance = maxRunDistance;
+        this.maxJumpHeight = maxJumpHeight;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/dev/astamur/geekbrains/lessons/lesson1/homework/Wall.java
@@ -0,0 +1,18 @@
+package dev.astamur.geekbrains.lessons.lesson1.homework;
+
+public class Wall implements Obstacle {
+    private final double height;
+
+    public Wall(double height) {
+        this.height = height;
+    }
+
+    public boolean perform(Athlete athlete) {
+        return athlete.jump(height);
+    }
+
+    @Override
+    public String toString() {
+        return "Wall " + height;
+    }
+}
