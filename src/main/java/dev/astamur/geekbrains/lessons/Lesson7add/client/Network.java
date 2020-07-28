db5f8c6bed7b5b588cff8538d2e1b365efd88cbc
Vladimir Rumyancev <53127742+Shakjamuni@users.noreply.github.com>
2020-07-28 10:19:47 +0300

    урок 7 чат историей сообщ.

--- src/main/java/dev/astamur/geekbrains/lessons/Lesson7add/client/Network.java
+++ src/main/java/dev/astamur/geekbrains/lessons/Lesson7add/client/Network.java
@@ -1,10 +1,16 @@
 package dev.astamur.geekbrains.lessons.Lesson7add.client;
 
-import java.io.Closeable;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.io.*;
 import java.net.Socket;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Network implements Closeable {
     private Socket socket;
@@ -15,6 +21,32 @@ public class Network implements Closeable {
     private Callback callOnAuthenticated;
     private Callback callOnException;
     private Callback callOnCloseConnection;
+    // объявление потока для записи
+    public FileWriter chatHistory;
+    //объявление потока для чтения.
+    public FileReader readChatHistory;
+    public int historyStringCounter = 0;
+    //сканер для прохода по строкам.
+    public Scanner chatScanner;
+    //список сообщений в истории чата который мы заполняем в конструкторе Network.
+    ArrayList<String> histToTextArea = new ArrayList<>();
+
+    //В конструкторе открываем файл в который будем писать историю сообщений.
+    public Network() throws IOException {
+        this.chatHistory = new FileWriter("chat.txt", true);
+        //открываем для чтения и подсчета строк истории.
+        this.readChatHistory = new FileReader("chat.txt");
+        this.chatScanner = new Scanner(readChatHistory);
+        while (chatScanner.hasNext()) {
+            historyStringCounter++;
+            histToTextArea.add(chatScanner.nextLine());
+        }
+        //оставляем в ArrayList только последние 100 сообщений для вывода в textArea в Controller.
+        if (historyStringCounter > 100) {
+            histToTextArea = (ArrayList) (histToTextArea.subList((histToTextArea.size() - 100), histToTextArea.size()));
+        }
+    }
+
 
     public void setCallOnMsgReceived(Callback callOnMsgReceived) {
         this.callOnMsgReceived = callOnMsgReceived;
@@ -81,13 +113,28 @@ public class Network implements Closeable {
         }
     }
 
+    //метод обращающийся к ArrayList histToTextArea для последовательного получения всех строк и передачи в javafx.TextArea
+    public void printHistory(javafx.scene.control.TextArea textArea) {
+        for (String s : histToTextArea) {
+            textArea.appendText(s + "\n");
+        }
+    }
+
     public boolean sendMsg(String msg) {
         if (out == null) {
             callOnException.callback("Соединение с сервером не установлено");
         }
-
         try {
             out.writeUTF(msg);
+            //блок отвечающий за запись сообщений в файл.
+            if (!msg.equals("/end")) {
+                LocalDateTime dateTime = LocalDateTime.now();
+                DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+                String toHistory = dateTime.format(myFormatObj) + " " + msg;
+                chatHistory.write(toHistory);
+                chatHistory.write("\n");
+            }
+            //.
             return true;
         } catch (IOException e) {
             e.printStackTrace();
@@ -98,7 +145,7 @@ public class Network implements Closeable {
     @Override
     public void close() {
         callOnCloseConnection.callback();
-        close(in, out, socket);
+        close(in, out, socket, chatHistory, readChatHistory, chatScanner);
     }
 
     private void close(Closeable... objects) {
@@ -110,4 +157,5 @@ public class Network implements Closeable {
             }
         }
     }
+
 }
